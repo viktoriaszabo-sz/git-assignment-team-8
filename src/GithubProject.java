b5387ef8bedf218a6bcf4fc6b93170a73b875915
Sujan Tiwari <sujantiwari1024@gmail.com>
2022-09-29 02:45:02 +0545

    Added simple program.

--- src/GithubProject.java
+++ src/GithubProject.java
@@ -1,15 +1,29 @@
-
+import java.util.Scanner;
 public class GithubProject {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {	
 
 		//Hi guys, you can add your comments here: 
 		
 		// Viktória Szabó's comment
 		// Sujan Tiwari's commnet
 		// Susha Andrei's comment
-		// Tiainen Jenina's comment
+		// Tiainen Jenina's commentpackage myproject;		
+		
+		int i, j, rows;
+		 Scanner myObj = new Scanner(System.in);
+		 System.out.println("How many rows?");
+		 rows = myObj.nextInt();
+		    
+		 for (i=0; i<rows; i++) {  
+		 for (j=(rows-i-2); j>=0; j--) {  
+		 System.out.print(" ");   
+		 }   
+		 for (j=0; j<=i; j++ ) {   
+		 System.out.print("*");   
+		 }   
+		 System.out.println();   
+		 } 		
 	}
 
 }
